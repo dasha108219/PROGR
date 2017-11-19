3e2e56c01b61bbeeeb8ab418518300393b9fc807
dasha108219 <31651439+dasha108219@users.noreply.github.com>
2017-11-19 16:13:07 +0300

    Delete 2.py

--- class/2.py
+++
@@ -1,8 +0,0 @@
-with open("new.txt", encoding = "utf-8") as file:
-    s = file.read()
-s = s.replace(",", "").replace(".", "")
-words = s.split()
-print(words)
-print(s.lower().count("па"))
-
-#generation выражения
