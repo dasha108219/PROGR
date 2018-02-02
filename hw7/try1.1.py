5c60d705e2009bdc027cc25535b08507e009b538
dasha108219 <31651439+dasha108219@users.noreply.github.com>
2018-02-02 19:13:02 +0300

    Add files via upload

---
+++ hw7/try1.1.py
@@ -0,0 +1,46 @@
+print('Вариант 2', "\n")
+
+## сколько в тексте разных существительных
+## с суффиксом -ness и какое существительное из них
+## имеет максимальную частотность.
+
+def words_with_ness(fname):
+    with open('1.txt', encoding = "utf-8") as file:
+        file = file.read()
+    file = file.lower()
+    symbols = '.,:;!?-""'''
+    for s in symbols:
+        file = file.replace(s, '')
+    words = file.split()
+    return words
+
+def word_freq(words):
+    counts = {}
+    for x in words:
+        if x[-4:] == 'ness':
+            if x in counts:
+                counts[x] += 1
+            else:
+                counts[x] = 1
+    return counts
+
+def values_pop(counts):
+    empty = []
+    maximum = max(counts.values())
+    for key, value in counts.items():
+        if value > maximum:
+            maximum = value
+            empty.append(key)
+        elif value == maximum:
+            empty.append(key)
+    return empty
+
+words = words_with_ness('1.txt')
+counts = word_freq(words)
+first = len(counts)
+second = values_pop(counts)
+
+print('Разных существительных с суффиксом -ness : ', first)
+print('Слово/слова с максимальной частотностью: ', second)
+
+
