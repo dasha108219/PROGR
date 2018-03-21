49a534d989cb3c426e5a29bdeb46c700d4125ffc
dasha108219 <31651439+dasha108219@users.noreply.github.com>
2018-03-21 10:34:53 +0300

    Add files via upload

---
+++ hw10/time-z-re.py
@@ -0,0 +1,56 @@
+print('Вариант 2\n')
+
+import re
+
+def make_file():
+    if fname == '':
+        print('Вы не ввели название файла')
+        exit(0)
+    with open(fname, encoding  = 'utf-8') as file:
+        file = file.read()
+    return file
+
+def city(file):
+    city = re.search(r'<title>(\w+\s*-*\s*\w*) — Википедия</title>', file)
+    return city
+
+def time_zone(file):
+    time = re.search( r'(UTC[+-−]\d+(?::\d\d)*)</a>(?:, <a .+>(UTC[+-−]\d+(?::\d\d)*)</a></span>)*', file, re.DOTALL)
+    if time is None:
+        print('Часовой пояс не указан')
+        exit(0)
+    return time
+
+def file_write(city, time):   
+    with open('time_zone.txt', 'a', encoding = 'utf-8') as new_file:
+        new_file.write(str(city[1]))
+        new_file.write(': ')                      # без groups(), чтобы выводилось не в скобочках, а красиво,                                                            
+        if time[2] is None:                       # и чтобы можно было работать с летним временем отдельно - выводить/не выводить
+            new_file.write(str(time[1]))
+        else:
+            new_file.write(str(time[1]))
+            new_file.write(', летом - ')
+            new_file.write(str(time[2]))
+        new_file.write('\n')
+    return new_file
+
+def to_print(city, time):
+    if time[2] is None:
+        pr = city[1]+ ': часовой пояс - '+ time[1]
+        return pr
+    else:
+        pr = city[1]+ ': часовой пояс - '+time[1] + ', летом - '+ time[2]
+        return pr
+
+fname = input('Введите название файла, который нужно открыть: ')
+file = make_file()
+city = city(file)
+time = time_zone(file)
+new_file = file_write(city, time)
+pr = to_print(city, time)
+
+print(pr)
+
+
+
+
