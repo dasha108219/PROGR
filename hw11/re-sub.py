aa0209ae6bf5731564f7c2d2bbc32cf1bffd4ddd
dasha108219 <31651439+dasha108219@users.noreply.github.com>
2018-03-29 11:03:20 +0300

    Add files via upload

---
+++ hw11/re-sub.py
@@ -0,0 +1,34 @@
+print('Вариант 2\n')
+
+import re
+
+# прилагательных нет
+
+def make_file():
+    if fname == '':
+        print('Вы не ввели название файла')
+        exit(0)
+    with open(fname, encoding  = 'utf-8') as file:
+        file = file.read()
+    return file
+
+def replace(file):
+    replace = re.sub(r'В[и́и]кинг', r'Бурундук', file) 
+    return replace
+
+def replace_sec(replace):
+    replace_sec= re.sub(r'викинг', r'бурундук', replace)
+    return replace_sec
+
+def file_write(replace_sec):   
+    with open('replace.txt', 'w', encoding = 'utf-8') as new_file:
+        new_file.write(str(replace_sec))
+    return new_file
+
+fname = input('Введите название файла, который нужно открыть: ')
+file = make_file()
+replace = replace(file)
+replace_sec = replace_sec(replace)
+new_file = file_write(replace_sec)
+
+print('Замена произведена!')
